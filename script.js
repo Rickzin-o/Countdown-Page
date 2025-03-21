55757b9f45153c4e17e9104100b7913c92ec67f4
José Ricardo <95586035+Rickzin-o@users.noreply.github.com>
2025-03-21 17:47:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+function getTimeDiv(time) {
+    let timeDiv = document.querySelector(`#${time}`)
+    return timeDiv.firstElementChild
+}
+
+
+function getRemainingTime(future) {
+    let now = new Date()
+    return new Date(future.getTime() - now.getTime())
+}
+
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+async function setCountdown() {
+    let inputElement = document.querySelector("#input-time")
+
+    if (inputElement.value == "") {return}
+
+    let futureTime = new Date(inputElement.value)
+    inputElement.value = ""
+    
+    document.body.firstElementChild.remove()
+
+    while (true) {
+        var time = getRemainingTime(futureTime)
+        getTimeDiv("days").innerHTML = (time / 86400000)|0
+        time %= 86400000
+        getTimeDiv("hours").innerHTML = (time / 3600000)|0
+        time %= 3600000
+        getTimeDiv("minutes").innerHTML = (time / 60000)|0
+        time %= 60000
+        getTimeDiv("seconds").innerHTML = (time / 1000)|0
+        await sleep(1000)
+    }
+}
